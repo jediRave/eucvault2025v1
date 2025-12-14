13921e56a8a194e797c1351632d2c0cad3f4a679
Jediah Matthew <jediah.m@gmail.com>
2025-12-14 01:32:54 -0500

    updated the app.py for Digitalocean error. error build job failed because it returned a non-zero exit code.

--- app.py
+++ app.py
@@ -43,5 +43,6 @@ def static_files(path):
     return send_from_directory(".", path)
 
 if __name__ == "__main__":
-    # local dev
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    port = int(os.environ.get("PORT", "8000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
